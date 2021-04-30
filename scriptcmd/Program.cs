05b10a033c7238b38d590d17665c27be645dafb9
Michael Balloni <balloni.michael@yahoo.com>
2021-04-29 20:01:05 -0700

    Added scriptcmd, moved from the app project

---
+++ scriptcmd/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace metascript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("RTFM!");
+                return;
+            }
+
+            string cmd = args[0].Trim();
+            string arg = "";
+            if (args.Length > 1)
+                arg = args[1];
+
+            try
+            {
+                using (var state = new HttpState(null))
+                {
+                    switch (cmd)
+                    {
+                        case "deleteuser":
+                            {
+                                string email = arg;
+                                Console.WriteLine("Email: " + email);
+                                if (!Email.IsEmailValid(email))
+                                {
+                                    Console.WriteLine("ERROR: Invalid email address!");
+                                    return;
+                                }
+                                Console.Write("Press [Enter] to delete - " + email + " - : ");
+                                Console.ReadLine();
+                                if (User.GetUserIdFromEmailAsync(state.MsCtxt, email).Result < 0)
+                                {
+                                    Console.WriteLine("ERROR: User not found with email " + email);
+                                    return;
+                                }
+                                User.DeleteUserAsync(state, email).Wait();
+                                Console.WriteLine("User deleted.");
+                            }
+                            break;
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                while (exp.InnerException != null)
+                    exp = exp.InnerException;
+                Console.WriteLine("EXCEPTION: " + exp);
+            }
+        }
+    }
+}
